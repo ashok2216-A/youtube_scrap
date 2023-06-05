18bef609479e7c4b1786960f2aae3b0c6ab8a6be
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2023-06-05 19:04:50 +0530

    Add files via upload

---
+++ apps/pages/Youtube_Comments_analysis.py
@@ -0,0 +1,49 @@
+import time
+import pprint
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import csv
+from youtube_comment_scraper_python import *
+import pandas as pd
+import plotly.express as px
+import re
+import streamlit as st
+
+st.markdown("# Page 3 🎉")
+st.sidebar.markdown("# Page 3 🎉")
+
+# url = input('Enter Youtube Video Url- ')
+# youtube.open(url)
+# youtube.keypress("pagedown")
+
+# data = []
+# currentpagesource=youtube.get_page_source()
+# lastpagesource=''
+
+# while(True):
+#     if(lastpagesource==currentpagesource):
+#         break
+        
+#     lastpagesource=currentpagesource
+#     response=youtube.video_comments()
+
+#     for c in response['body']:
+#         data.append(c)
+        
+#     youtube.scroll()
+#     currentpagesource=youtube.get_page_source()
+
+
+# df = pd.DataFrame(data)
+
+# df = df.replace('\n',' ', regex=True)
+
+# df = df[['Comment', 'Likes']].drop_duplicates(keep="first") 
+# # df = df[['Likes']].drop_duplicates(keep="first") 
+
+# df.to_csv('output/data.csv',index=False) 
+
+# df.head()
---
+++ apps/pages/Youtube_analysis.py
@@ -0,0 +1,38 @@
+import time
+import pprint
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+import csv
+from youtube_comment_scraper_python import *
+import pandas as pd
+import plotly.express as px
+import re
+import streamlit as st
+import numpy as np
+
+st.title('Youtube Channel Analysis📈')
+
+df = pd.read_csv(r'C:/Users/ashok/OneDrive/Desktop/WebScrap/Youtube/output/people.csv')
+st.dataframe(df)
+
+count = st.slider('Select Lower Video Count', 0, len(df), 100)
+st.write("You selected", count, 'Videos')
+
+
+fig = px.bar(df[:count],
+    x="title",
+    y="views", height=1000
+)
+fig.update_traces(textfont_size=12, textangle=0, textposition="outside", cliponaxis=False)
+# fig.update_yaxes(tickvals=['10k', '22k', '29k', '56k'])
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+with tab1:
+    # Use the Streamlit theme.
+    # This is the default. So you can also omit the theme argument.
+    st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+with tab2:
+    # Use the native Plotly theme.
+    st.plotly_chart(fig, theme=None, use_container_width=True)
