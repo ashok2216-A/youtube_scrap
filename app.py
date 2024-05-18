9bbda311d7398927de12ba5b9adffcb0b842922f
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-05-18 12:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import csv
