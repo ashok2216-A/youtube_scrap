baecb51c21526fea36410ce2bfac4bbf082051d7
Ashok_kumar <74713336+ashok2216-A@users.noreply.github.com>
2024-05-18 13:16:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,20 +20,20 @@ import streamlit as st
 st.title('Youtube WebScrap⛏️')
 
 # # ------------------------------------------------------------------------------CHANNEL DATA------------------------------------------------------------------------
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+# options = Options()
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# chromedriver_autoinstaller.install()
-# # driver = webdriver.Chrome('/usr/bin/google-chrome') 
-# chrome_path = '/usr/lib/chromium-browser/chromedriver'
+chromedriver_autoinstaller.install()
+# driver = webdriver.Chrome('/usr/bin/chromedriver') 
+chrome_path = '/usr/bin/chromedriver'
 # # Set up Chrome options if needed
-# chrome_options = webdriver.ChromeOptions()
+chrome_options = webdriver.ChromeOptions()
 # # Create the WebDriver instance
-# chrome_options.binary_location = chrome_path
-# driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
+chrome_options.binary_location = chrome_path
+driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
 # driver = webdriver.Chrome(
 #     service=ChromeService(ChromeDriverManager().install()))
 # driver.implicitly_wait(5)
